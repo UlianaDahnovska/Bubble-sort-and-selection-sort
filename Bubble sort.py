a785079553da20d9d28925eebc3e9a11c902f333
UlianaDahnovska <55241559+UlianaDahnovska@users.noreply.github.com>
2020-05-30 21:33:58 +0300

    Update Bubble sort.py

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,11 +1,15 @@
 def bubble_sort(nums):
+#Ми встановлюємо swapped на True, щоб цикл відбувався хоч раз
     swapped = True
     while swapped:
         swapped = False
         for i in range(len(nums) - 1):
             if nums[i] > nums[i + 1]:
+                #Міняємо елементи місцями
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                #swapped встановлюємо на True на продовжуємо цикл
                 swapped = True
+#Приклад
 random_list_of_nums = [10, 6, 38, 8, 34]
 bubble_sort(random_list_of_nums)
 print(random_list_of_nums)
